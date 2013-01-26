e3d3542af395a7e250d8347ab050ed2ab2d4f89d
tedder <ted@perljam.net>
2013-01-25 16:39:29 -0800

    first stub, baby little Runner class.

---
+++ HangmanSolver/src/net/inervo/HangmanSolver/Runner.java
@@ -0,0 +1,20 @@
+package net.inervo.HangmanSolver;
+
+public class Runner implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world.");
+		new Runner().run();
+	}
+
+}
